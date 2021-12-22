1b1400435937ba6acea3aa1c64d781d3a9f755b8
aausi <aausi@DESKTOP-2JVPSSP>
2021-12-23 01:37:05 +0300

    add more objects to XML file

--- src/test/java/com/epam/gem/ParserSAXTest.java
+++ src/test/java/com/epam/gem/ParserSAXTest.java
@@ -9,6 +9,19 @@ import java.util.List;
 public class ParserSAXTest {
     private static final String VALID_PATH = "C:\\Users\\aausi\\Documents\\Task2\\src\\test\\resources\\xml";
     private static final String INVALID_PATH = "";
+    private static final List<Gem> GEMS = Arrays.asList(
+            new Gem(1, "star ruby", Preciousness.PRECIOUS, "Myanmar",
+                    5, 1),
+            new Gem(2, "Colombian emerald", Preciousness.SEMIPRECIOUS, "Colombia",
+                    20, 2),
+            new Diamond(3, "white diamond", Preciousness.PRECIOUS, "Botswana",
+                    50, 0.5, CrystalShape.OCTAHEDRAL),
+            new Diamond(4, "blue diamond", Preciousness.PRECIOUS, "South Africa",
+                    90, 0.75, CrystalShape.CUBIC),
+            new Sapphire(5, "blue sapphire", Preciousness.SEMIPRECIOUS, "Australia",
+                    75, 1.5, "blue"),
+            new Sapphire(6, "pink sapphire", Preciousness.SEMIPRECIOUS, "Thailand",
+                    80, 1.25, "pink"));
 
     @Test
     public void testParseShouldParseWhenPathValid() throws GemException {
@@ -16,19 +29,10 @@ public class ParserSAXTest {
         ParserSAX parser = new ParserSAX();
 
         //when
-        List<Gem> actual = parser.parse(VALID_PATH);
-        List<Gem> expected = Arrays.asList(
-                new Gem(1, "star_ruby", Preciousness.PRECIOUS, "Myanmar",
-                        5, 1),
-                new Gem(2, "colombian_emerald", Preciousness.SEMIPRECIOUS, "Colombia",
-                        20, 2),
-                new Diamond(3, "white_diamond", Preciousness.PRECIOUS, "Botswana",
-                        50, 0.5, CrystalShape.OCTAHEDRAL),
-                new Sapphire(4, "blue_sapphire", Preciousness.SEMIPRECIOUS, "Australia",
-                        75, 1.5, "blue"));
+        List<Gem> result = parser.parse(VALID_PATH);
 
         //then
-        Assert.assertEquals(expected, actual);
+        Assert.assertEquals(GEMS, result);
     }
 
     @Test(expected = GemException.class)
